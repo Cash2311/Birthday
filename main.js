b2e04227df2b91f491ff15f33a7ebb75b6522879
Cash2311 <86266624+Cash2311@users.noreply.github.com>
2022-10-28 18:53:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song1 = "";
+
+function preload()
+{
+    song1 = loadSound("happy-birthday-to-you-traditional-song.mp3");
+}
+function setup()
+{}
+    
+
+
+
+function play()
+{
+    song1.play();
+}
